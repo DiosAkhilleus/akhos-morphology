396dbbf9fb387e3d8edf704fd58c432c290bb286
DiosAkhilleus <abertin1809@gmail.com>
2021-03-15 17:12:46 -0500

    changed form event listener

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-
 import {  getMorph  } from './morph';
 
 function handleForm(event) {  event.preventDefault();  }
 
-async function getFull (input) {
-    const morph = await getMorph(input);
+async function getFull () {
+    let formData = document.querySelector('#lemmaform input');  
+    let lemma = formData.value;
+    const morph = await getMorph(lemma);
     console.log(morph);
 }
 
 let form = document.getElementById('lemmaform');
 form.addEventListener('submit', handleForm);
-form.addEventListener('submit', test);
-
-function test () {
-    let formData = document.querySelector('#lemmaform input');  
-    let newBook = formData.value;
-    getFull(newBook);
-}
+form.addEventListener('submit', getFull);
 
