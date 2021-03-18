a8b5a6858758836002cf42a6ece97c56283f1f93
DiosAkhilleus <abertin1809@gmail.com>
2021-03-17 20:25:17 -0500

    if input word has multiple headword possibilities, getGreekMorph will return an array with each possible

--- src/greek.js
+++ src/greek.js
@@ -1,21 +1,53 @@
-async function getMorph (lemma) { // fetches the given greek string from the morphology service
+async function getGreekMorph (lemma) { // fetches the given greek string from the morphology service
     const greekData = await fetch(`http://services.perseids.org/bsp/morphologyservice/analysis/word?lang=grc&engine=morpheusgrc&word=${lemma}`, {mode: 'cors'});
     const dataOut = await greekData.json();
+    const body = dataOut.RDF.Annotation.Body;
     console.log(dataOut);
-    let type;
-    if(dataOut.RDF.Annotation.Body.rest.entry.infl[0] !== undefined){
-        type = (dataOut.RDF.Annotation.Body.rest.entry.infl[0].pofs.$);
-    } else {
-        type = dataOut.RDF.Annotation.Body.rest.entry.infl.pofs.$;
-    }
 
-    const inflections = dataOut.RDF.Annotation.Body.rest.entry.infl;
-    const headWord = dataOut.RDF.Annotation.Body.rest.entry.dict.hdwd.$;
-    const inflect = getInflectionStates(inflections, type);
-    const dict = await getDict(headWord);
-    return [headWord, inflect, dict];
+    let type;
+    let returnArr = [];
+    if(Array.isArray(body)){
+        let subArr = [];
+        for(let i = 0; i < body.length; i++){
+            if(dataOut.RDF.Annotation.Body[i].rest.entry.infl[0] !== undefined){
+                type = (dataOut.RDF.Annotation.Body[i].rest.entry.infl[0].pofs.$);
+            } else {
+                type = dataOut.RDF.Annotation.Body[i].rest.entry.infl.pofs.$;
+            }
+            const inflections = dataOut.RDF.Annotation.Body[i].rest.entry.infl;
+            const headWord = dataOut.RDF.Annotation.Body[i].rest.entry.dict.hdwd.$;
+            const inflect = getGreekInflections(inflections, type);
+            const dict = await getDict(headWord);
+
+            if(inflect === undefined){
+                subArr = [headWord, type, dict];
+            } else {
+                subArr = [headWord, type, inflect, dict];
+            }
+            returnArr.push(subArr);
+        }
+        return returnArr;
+    } else {
+        
+        if(dataOut.RDF.Annotation.Body.rest.entry.infl[0] !== undefined){
+            type = (dataOut.RDF.Annotation.Body.rest.entry.infl[0].pofs.$);
+        } else {
+            type = dataOut.RDF.Annotation.Body.rest.entry.infl.pofs.$;
+        }
+    
+        const inflections = dataOut.RDF.Annotation.Body.rest.entry.infl;
+        const headWord = dataOut.RDF.Annotation.Body.rest.entry.dict.hdwd.$;
+        const inflect = getGreekInflections(inflections, type);
+        const dict = await getDict(headWord);
+
+        if(inflect === undefined){
+            return [headWord, type, dict];
+        } else {
+            return [headWord, type, inflect, dict];
+        }   
+    }
 };
-const getInflectionStates = (inflectArr, type) => { // returns an array in which each element is itself an array of the dialect type and inflection pattern
+const getGreekInflections = (inflectArr, type) => { // returns an array in which each element is itself an array of the dialect type and inflection pattern
     
     if (type === 'verb'){
         if(Array.isArray(inflectArr)){
@@ -29,9 +61,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
                     let mood = inflectArr[i].mood.$;
                     if(inflectArr[i].dial){
                         let dialect = inflectArr[i].dial.$;
-                        combinedArr[i] = [[`${dialect}`], [`${person} person ${number} ${tense} ${voice} ${mood}`]];
+                        combinedArr[i] = [`${dialect}`, `${person} person ${number} ${tense} ${voice} ${mood}`];
                     }else {
-                        combinedArr[i] = [['attic'], [`${person} person ${number} ${tense} ${voice} ${mood}`]];
+                        combinedArr[i] = ['attic', `${person} person ${number} ${tense} ${voice} ${mood}`];
                     }
                 }
             return combinedArr;
@@ -43,9 +75,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
             let mood = inflectArr.mood.$;
             if(inflectArr.dial){
                 let dialect = inflectArr.dial.$;
-                return [[`${dialect}`], [`${person} person ${number} ${tense} ${voice} ${mood}`]];
+                return [`${dialect}`, `${person} person ${number} ${tense} ${voice} ${mood}`];
             }else {
-                return [['attic'], [`${person} person ${number} ${tense} ${voice} ${mood}`]];
+                return ['attic', `${person} person ${number} ${tense} ${voice} ${mood}`];
             }
         }
     } if (type === 'verb participle'){
@@ -59,9 +91,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
                     let mood = inflectArr[i].mood.$;
                     if(inflectArr[i].dial){
                         let dialect = inflectArr[i].dial.$;
-                        combinedArr[i] = [[`${dialect}`], [`${gender} ${number} ${tense} ${voice} ${mood}`]];
+                        combinedArr[i] = [`${dialect}`, `${gender} ${number} ${tense} ${voice} ${mood}`];
                     }else {
-                        combinedArr[i] = [['attic'], [`${gender} ${number} ${tense} ${voice} ${mood}`]];
+                        combinedArr[i] = ['attic', `${gender} ${number} ${tense} ${voice} ${mood}`];
                     }
                 }
             return combinedArr;
@@ -73,9 +105,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
             let mood = inflectArr.mood.$;
             if(inflectArr.dial){
                 let dialect = inflectArr.dial.$;
-                return [[`${dialect}`], [`${gender} ${number} ${tense} ${voice} ${mood}`]];
+                return [`${dialect}`, `${gender} ${number} ${tense} ${voice} ${mood}`];
             }else {
-                return [['attic'], [`${gender} ${number} ${tense} ${voice} ${mood}`]];
+                return ['attic', `${gender} ${number} ${tense} ${voice} ${mood}`];
             }
         }
     } if (type === 'noun'){
@@ -89,9 +121,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
                     let declension = inflectArr[i].decl.$;
                     if(inflectArr[i].dial){
                         let dialect = inflectArr[i].dial.$;
-                        combinedArr[i] = [[`${dialect}`], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+                        combinedArr[i] = [`${dialect}`, `${gender} ${nCase} ${number}, ${declension} declension`];
                     }else {
-                        combinedArr[i] = [['attic'], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+                        combinedArr[i] = ['attic', `${gender} ${nCase} ${number}, ${declension} declension`];
                     }
                 }
             return combinedArr;
@@ -102,9 +134,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
         let declension = inflectArr.decl.$;
         if(inflectArr.dial){
             let dialect = inflectArr.dial.$;
-            return [[`${dialect}`], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+            return [`${dialect}`, `${gender} ${nCase} ${number}, ${declension} declension`];
         }else {
-            return [['attic'], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+            return ['attic', `${gender} ${nCase} ${number}, ${declension} declension`];
         }
     } if (type === 'adjective'){
         if(Array.isArray(inflectArr)){
@@ -117,9 +149,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
                     let declension = inflectArr[i].decl.$;
                     if(inflectArr[i].dial){
                         let dialect = inflectArr[i].dial.$;
-                        combinedArr[i] = [[`${dialect}`], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+                        combinedArr[i] = [`${dialect}`, `${gender} ${nCase} ${number}, ${declension} declension`];
                     }else {
-                        combinedArr[i] = [['attic'], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+                        combinedArr[i] = ['attic', `${gender} ${nCase} ${number}, ${declension} declension`];
                     }
                 }
             return combinedArr;
@@ -130,9 +162,9 @@ const getInflectionStates = (inflectArr, type) => { // returns an array in which
         let declension = inflectArr.decl.$;
         if(inflectArr.dial){
             let dialect = inflectArr.dial.$;
-            return [[`${dialect}`], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+            return [`${dialect}`, `${gender} ${nCase} ${number}, ${declension} declension`];
         }else {
-            return [['attic'], [`${gender} ${nCase} ${number}, ${declension} declension`]];
+            return ['attic', `${gender} ${nCase} ${number}, ${declension} declension`];
         }
     }
 };
@@ -140,16 +172,26 @@ async function getDict (lemma) {
     const dictEntry = await fetch(`https://en.wiktionary.org/api/rest_v1/page/definition/${lemma}`, {mode: 'cors'});
     const entryOut = await dictEntry.json();
     
-    let def = entryOut.other[0].definitions[0].definition;
-     
-    document.getElementById('greek').innerHTML = def;
-
-    let titles = document.querySelectorAll('#greek a');
-    let sumDef = `${titles[0].textContent}`;
-    for(let i = 1; i < titles.length; i++){
-        sumDef = sumDef + `, ${titles[i].textContent}`;
+    if(entryOut.other !== undefined){
+        let def;
+        let defArr = entryOut.other[0].definitions
+        if(entryOut.other[0].definitions.length = 1){
+            def = entryOut.other[0].definitions[0].definition;
+        } else {
+            for(let i = 0; i < defArr.length; i++ ){
+                def = def + defArr[i].definition; 
+            }
+        }
+        document.getElementById('greek').innerHTML = def;
+    
+        let titles = document.querySelectorAll('#greek a');
+        let sumDef = `${titles[0].textContent}`;
+        for(let i = 1; i < titles.length; i++){
+            sumDef = sumDef + `, ${titles[i].textContent}`;
+        }
+        return sumDef;
     }
-    return sumDef;
+    return "Definition Not Found";
 };
 
-export {  getMorph  };
+export {  getGreekMorph  };
